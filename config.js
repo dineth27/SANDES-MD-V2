8847ad3a853fc6f1bbd1fc829a7f9c9c409da845
dineth27 <Dinethnethum533@gmail.com>
2026-09-18 17:13:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSON-ID_HERE",
+SESSION_ID: process.env.SESSION_ID || "RQlVCDrZ#a3J1dmXpBmzrrd34Qf8URShdg33cf-hbFDUXKy_1JwI",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
